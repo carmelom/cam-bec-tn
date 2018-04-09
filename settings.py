6e6f5ead2f49879584ca3bfb31dc26c1961c9c81
carmelom <carmelo.mordini@gmail.com>
2018-04-09 11:40:28 +0200

    Ding must be killed

--- settings.py
+++ settings.py
@@ -1,28 +1,23 @@
 import os
 
 #basedir = r"c:/fit/siscam"
-#basedir = os.getcwd()
-basedir = r"c:/SIScam/SIScamProgram/Prog"
+basedir = "/home/bec/CAM"
 
 #full path to image file
-#imagefile = r"c:/fit/ocf/fit.sis"
-#imagefile = r"./img/test.sis"
-imagefile = r"c:/SIScam/SIScamProgram/Prog/img/test.sis"
-#imagefile = r"./img/fit.sis"
-#imagefile = r"./img/20100319-LBside-0010.sis"
-#rawimage1file = r"c:/fit/ocf/rawimg1.sis"
-#rawimage2file = r"c:/fit/ocf/rawimg2.sis"
-#rawimage3file = r"c:/fit/ocf/rawimg3.sis"
+
+imagefile = os.path.join(basedir, 'img/test.sis')
+
 rawimage1file = imagefile
 rawimage2file = imagefile
 rawimage3file = imagefile
 
-referencefile = r"c:/SIScam/SIScamProgram/Prog/img/reference.sis"
+referencefile = os.path.join(basedir, 'img/reference.sis')
 
 #where to save images
 #imagesavepath = r"./img"
-#imagesavepath = r"d:/SIScam/SIScamProgram/Prog/img"
-imagesavepath = os.path.normpath("d:/SIScam/SIScamProgram/Prog/img")
+#imagesavepath = os.path.join(basedir, 'img')
+
+imagesavepath = '/media/bec/Data/SIScam/SIScamProgram/Prog/img'
 
 #icons, etc.
 bitmappath = os.path.join(basedir, 'bitmaps')
@@ -39,8 +34,6 @@ useSony = False
 
 #settings for Theta Systems cam
 #configfile = r"c:\WinSIS6\py\config.ini"
-#configfile = r"./WinSIS6/py/config.ini"
-configfile = r'c:/SIScam/SIScamProgram/Prog/config.ini'
-
+configfile = r"./WinSIS6/py/config.ini"
 #usePseudoCam = False
 usePseudoCam = True
